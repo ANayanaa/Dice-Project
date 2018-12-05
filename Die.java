4f92933b4150ab838cbb1a1bf2f5e477cdf88b41
aryanjairath <aryanjairath@users.noreply.github.com>
2018-12-05 07:59:13 -0500

    This One Actually Works

--- Die.java
+++ Die.java
@@ -2,18 +2,15 @@ public class Die{
 	private int val;
 	private int side;
 		public Die(){
-			val = 0;
+			val = (int)(Math.random()*6)+1;
 			side = 6;
 		}
 		public Die(int val, int side){
-			this.val = val;
+			this.val = (int)(Math.random()*6)+1;
 			this.side = side;
 		}
 		public int getValue(){
 			return val;
-		}
-		public int getSide(){
-			return side;
 		}
 		public void roll(){
 			val = (int)(Math.random()*6)+1;
