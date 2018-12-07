3371b01b6e2508c259a39299577c4f25500b91b4
Arnitha <45392215+ANayanaa@users.noreply.github.com>
2018-12-07 09:04:08 -0500

    added shake to runner

--- DiceRunner.java
+++ DiceRunner.java
@@ -7,8 +7,7 @@ public class DiceRunner{
 		ArrayList <Die> list = new ArrayList<>();
 
 		DiceHolder diceholder = new DiceHolder();
-		int c = 1;
-		do{
+		for(int i = 0; i<10;i++){
 				int sizes = (int)(Math.random()*20)+4;
 				int value = (int)(Math.random()*(sizes-1))+1;
 			Die dices = new Die(value,sizes);
@@ -16,8 +15,13 @@ public class DiceRunner{
 			diceholder.addDie(new Die());
 			System.out.println(diceholder);
 
-}while(c==1);
 
+
+
+}
+System.out.println("after shaking");
+diceholder.shake();
+System.out.println(diceholder);
 
 	}
 }
